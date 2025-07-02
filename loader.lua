2c0cf6f403240fd8841d6de8cd8a7e9f37e06c80
DeadMark666X <adityamahardikaputra4@gmail.com>
2025-07-02 11:51:35 +0900

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,4 @@
--- loader.lua
-local uiScript = loadstring(game:HttpGet("https://raw.githubusercontent.com/DeadMark666X/MarkHub/main/Ui.lua"))()
-uiScript:Init()
+-- loader.lua (versi final yang ringan dan bersih)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DeadMark666X/MarkHub/main/Ui.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DeadMark666X/MarkHub/main/autoaim.lua"))()
+-- loadstring(game:HttpGet(".../esp.lua"))() -- (nanti kalau kamu sudah punya ESP)

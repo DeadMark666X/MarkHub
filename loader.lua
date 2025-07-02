e1bb394d026ddd610e181128e8f8f6d8f221819c
DeadMark666X <adityamahardikaputra4@gmail.com>
2025-07-02 12:05:16 +0900

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,2 @@
--- loader.lua (versi final yang ringan dan bersih)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DeadMark666X/MarkHub/main/Ui.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DeadMark666X/MarkHub/main/autoaim.lua"))()
--- loadstring(game:HttpGet(".../esp.lua"))() -- (nanti kalau kamu sudah punya ESP)

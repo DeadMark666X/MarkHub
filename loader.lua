ccc9434f3dd9afde2b22cc68b4605abaf698c925
DeadMark666X <adityamahardikaputra4@gmail.com>
2025-07-02 12:56:55 +0900

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DeadMark666X/MarkHub/main/Ui.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DeadMark666X/MarkHub/main/autoaim.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DeadMark666X/MarkHub/main/esp.lua"))()
